7b768b1a952e1023dff3c84f0998c0689b472f93
JoaquinOnSoft <joaquingp@gmail.com>
2021-06-22 15:27:41 +0200

    Added license header.

--- src/com/opentext/otmm/sc/eventlistener/handler/ApplyWatermarkToLimitedUseAssetOnDownload.java
+++ src/com/opentext/otmm/sc/eventlistener/handler/ApplyWatermarkToLimitedUseAssetOnDownload.java
@@ -1,6 +1,22 @@
+/*
+ * (C) Copyright 2021 Joaquín Garzón (http://opentext.com) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *   Joaquín Garzón - initial implementation
+ */
 package com.opentext.otmm.sc.eventlistener.handler;
-
-import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -13,9 +29,7 @@ import com.artesia.event.Event;
 import com.artesia.metadata.MetadataCollection;
 import com.artesia.metadata.MetadataField;
 import com.artesia.security.SecuritySession;
-import com.opentext.job.Job;
 import com.opentext.otmm.sc.eventlistener.OTMMField;
-import com.opentext.otmm.sc.eventlistener.helper.JobHelper;
 import com.opentext.otmm.sc.eventlistener.helper.SecurityHelper;
 
 public class ApplyWatermarkToLimitedUseAssetOnDownload implements OTMMEventHandler {
